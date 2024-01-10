3f5fefdd5fed95d8c4acace1bb874bd704328980
Rasmus Ilmjärv <rasmus.ilmjarv@gmail.com>
2024-01-10 19:22:23 +0200

    new cards to display project WIP

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="flex flex-col gap-4">
-        <div class="border-t" v-for="item in data">
-            <a :href="item.link">{{item.title}}</a>
-            <div>{{ item.description }}</div>
-        </div>
+    <div class="grid xl:grid-cols-2 gap-4">
+        <ProjectCard :data="data" />
     </div>
 </template>
 <script setup lang="ts">
@@ -16,6 +13,11 @@ description: 'A simple, lightweight solution to download images from the web. It
 title: 'Genshin Impact ingame satistics viewer',
 link: 'https://github.com/rasmusilm/genshin-stats-tracker',
 description: ' A small project written to make it easier to see ingame state from console without starting the game, and automate the "outside the game" reward mechanics: getting the daily login rewards and searching + redeeming giftcodes for ingame rewards. An exercise in using and reverse engineering api-s.'
-}
+},
+{
+title: 'This site',
+link: 'https://github.com/rasmusilm/rasmusilm.github.io',
+description: 'A Nuxt3 app served with github pages using SSR (Server Side Rendering)'
+},
 ])
 </script>
